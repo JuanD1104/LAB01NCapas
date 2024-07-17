31809692c58c35025bf7a473edaa50f166920b28
Juan <juanmurcia893@gmail.com>
2024-07-17 14:48:43 -0500

    feat(git) Add .gitignore and .gitattributes

---
+++ Orders/DAL/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+
+namespace DAL
+{
+    namespace Models
+    {
+        public interface IRepository : IDisposable
+        {
+            Task<TEntity> CreateAsync<TEntity>(TEntity toCreate) where TEntity : class;
+
+            Task<bool> DeleteAsync<TEntity>(TEntity toDelete) where TEntity : class;
+
+            Task<bool> UpdateAsync<TEntity>(TEntity toUpdate) where TEntity : class;
+
+            Task<TEntity> RetreiveAsync<TEntity>(Expression<Func<TEntity, bool>> criteria) where TEntity : class;
+
+            Task<List<TEntity>> FilterAsync<TEntity>(Expression<Func<TEntity, bool>> criteria) where TEntity : class;
+        }
+    }
+
+}
